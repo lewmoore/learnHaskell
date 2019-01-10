0b5108810ffef55d4a53da768067f0136ce83302
Lewis Moore <lewis.moore2707@gmail.com>
2019-01-10 09:13:44 +0000

    feat(haskell): first learnings

---
+++ haskellIntro.hs
@@ -0,0 +1,48 @@
+chuckNorris :: (Integral a) => a -> String
+chuckNorris 2 = "Not chuck Norris"
+chuckNorris 1 = "chuck norris"
+
+add :: Integer -> Integer -> Integer
+add x y = x + y
+
+addThree :: Integer -> Integer -> Integer -> Integer
+addThree x y z = x + y + z
+
+-- third :: (a, b, c) -> c
+-- third (_, _, c) = c
+
+
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+addTripleVector :: (Num a) => (a, a, a) -> (a, a, a) -> (a, a, a)
+addTripleVector (x1, y1, z1) (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2)
+
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi <= 18.56 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+
+wealthLevel :: String -> String
+wealthLevel money
+    | money <= "£10" = "Broke"
+    | money <= "£100" = "Less Broke"
+    | money <= "£1000" = "Not Broke"
+    | money <= "£10000" = "Rich"
+    | otherwise = "Minted"
+
+myCompare :: (Ord a) => a -> a -> Ordering  
+a `myCompare` b  
+    | a > b     = GT  
+    | a == b    = EQ  
+    | otherwise = LT 
+
+initials :: String -> String -> String  
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."  
+    where (f:_) = firstname  
+          (l:_) = lastname   
+             
