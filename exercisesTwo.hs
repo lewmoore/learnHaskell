16542be13df84500c4af4ed900011fa9731253be
Lewis Moore <lewis.moore2707@gmail.com>
2019-01-11 09:28:49 +0000

    Add exercise 3.4

--- exercisesTwo.hs
+++ exercisesTwo.hs
@@ -6,3 +6,19 @@ exOr :: Bool -> Bool -> Bool
 exOr True x = not x
 exOr False x = x
 
+-- 3.2 Give the 'box and line' diagram corresponding to your answer to the previous question. 
+
+--3.3 Using literals on the left-hand side we can make the truth table for a function
+--into its Haskell definition. Complete the following definition of exOr in this style.
+
+
+-- 3.4 Give two different definitions of the nAnd function which returns the result True except when both its arguments are True. Give a
+-- diagram illustrating one of your definitions. 
+
+nAnd :: Bool -> Bool -> Bool
+nAnd True True = False
+nAnd True False = True
+nAnd True True = True
+nAnd False False = True
+
+
